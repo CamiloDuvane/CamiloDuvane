e06b76d91d9166b358c3862891fc84f38bf193bc
Camilo Duvane <camilowilliam0@gmail.com>
2025-01-31 15:07:11 +0200

    Delete apiService.ts

--- apiService.ts
+++
@@ -1,7 +0,0 @@
-export abstract class ApiService {
-    protected baseUrl: string;
-
-    constructor() {
-        this.baseUrl = process.env.VUE_APP_API_BASE_URL || 'http://localhost:3100';
-    }
-}
