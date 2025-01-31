da988fde56ecf94d1b6de87cf164580f1c18c3bb
Camilo Duvane <camilowilliam0@gmail.com>
2025-01-31 15:07:22 +0200

    Delete app.config.ts

--- app.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineAppConfig({
-    ui: {
-        primary: "amber",
-        gray: "cool",
-        notifications: {
-            position: 'top-0 bottom-[unset]'
-        }
-    },
-});
