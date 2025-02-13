9a48e96311d9b480901734dfc2f1676c0289951d
Camilo Duvane <camilowilliam0@gmail.com>
2025-02-13 10:53:03 +0200

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -6,11 +6,12 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAgR2dt428kGYnlVeDFhzDfL7aDrPvd2_s",
   authDomain: "cwd2025-a050d.firebaseapp.com",
+  databaseURL: "https://cwd2025-a050d-default-rtdb.firebaseio.com",
   projectId: "cwd2025-a050d",
   storageBucket: "cwd2025-a050d.firebasestorage.app",
   messagingSenderId: "187470777488",
-  appId: "1:187470777488:web:5cf9c0397d0a2d1c119c15",
-  measurementId: "G-NBRPD1Z4EE"
+  appId: "1:187470777488:web:1964c6c3a40b053c119c15",
+  measurementId: "G-TDLMHMHB3H"
 };
 
 // Initialize Firebase
